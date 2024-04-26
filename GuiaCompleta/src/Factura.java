19464c29f34d188626e83e5974a9618186b61520
405660-NIETO <405660@tecnicatura.frc.utn.edu.ar>
2024-04-26 01:53:07 -0300

    Factura incompleta

---
+++ GuiaCompleta/src/Factura.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Factura {
+    public void listaCompras(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\n");
+        System.out.println("\tBienvenido al programa Factura");
+        System.out.println("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\n");
+        System.out.println("> Ingrese porfavor el precio y cantidad <");
+        System.out.println("\t# Primer Articulo #");
+        System.out.println("\tIngrese $ Precio");
+        Double[][] articulos = new Double[2][3]; //{{precio,cantidad} x3 }
+    }
+}
