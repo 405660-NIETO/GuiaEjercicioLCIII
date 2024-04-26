578dc0749d5690fbf1045fc029e9996906f02e94
405660-NIETO <405660@tecnicatura.frc.utn.edu.ar>
2024-04-26 17:44:57 -0300

    Refactorizacion de Codigo en el Menu.
    Ejercicios 1, 2 subdivididos en metodos con opciones de menu refactorizadas

--- GuiaCompleta/src/Main.java
+++ GuiaCompleta/src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main
@@ -8,67 +9,39 @@ public class Main
         Scanner scanner = new Scanner(System.in);
         boolean choice = true;
         boolean continuar = true;
-        System.out.println("\nBievenido a la guia completa de Labo3");
+        System.out.println("\nBievenido a la guia completa de LC-III");
         System.out.println("--------------------------------------");
         int eleccion = 1;
-        int num1, num2, num3;
-        double d1, d2, d3;
-        String a,b,c;
 
-
-        while(continuar) //Para repetir todo el programa
+        while(continuar) //Para repetir el programa completo
         {
             while (choice)  //Por si se arrepiente del ejercicio que eligió
             {
-
                 System.out.println("\nElige a qué menu de ejercicio deseas acceder : 1 | 2 | 3\n");
                 eleccion = scanner.nextInt();
-                System.out.println("\nQuieres acceder a la consola del ejercicio " + eleccion + "?\n");
-                System.out.println("Y/N\n");
-                scanner.nextLine(); //Ahí limpie el bufer
-                a = scanner.nextLine();
-                if (a.equals("Y") || a.equals("y"))
-                {
-                    choice = false; //No reiniciar el bucle
-                }
+                scanner.nextLine(); // limpiar por el int de arriba
+                choice = menuEjercicio(scanner,eleccion);
             }
             switch (eleccion) {
                 case 1:
-                    System.out.println("-----------------------------------");
-                    System.out.println("\nEjercicio 1");
-                    System.out.println("\nIngrese 2 numeros. Se procederá a calcular la respectiva Suma, Resta," +
-                            " Multiplicacion, Division y Resto de ambos numeros\n");
-                    System.out.println("Primer numero: ");
-                    num1 = scanner.nextInt();
-                    System.out.println("Segundo numero: ");
-                    num2 = scanner.nextInt();
-
-                    System.out.println("< Resultados >");
-                    metodos.operaciones(num1, num2);
-                    System.out.println("-----------------------------------");
-
-                    continuar = desea_continuar(); //Metodo que pregunta si quiere otro ejercicio
+                    ejercicio1(scanner, metodos); //ejercicio 1
+                    continuar = volverMenu(); //Metodo que pregunta si quiere otro ejercicio
                     if (continuar){
                         choice = true; //Habilita el bucle de opcion de ejercicio
                     }
                     break;
 
                 case 2: //Este no tiene reiniciar
-                    System.out.println("-----------------------------------");
-                    System.out.println("\nEjercicio 2");
-                    System.out.println("Ingrese un monto en $$ (double) y se procederá a calcular el IVA del mismo\n");
-                    d1 = scanner.nextDouble();
-                    System.out.println("El monto $"+d1+ " con el IVA aplicado es de $"+metodos.agregarIVA(d1));
-                    System.out.println("-----------------------------------");
+                    ejercicio2(scanner, metodos);
 
-                    continuar = desea_continuar(); //Metodo que pregunta si quiere otro ejercicio
+                    continuar = volverMenu(); //Metodo que pregunta si quiere otro ejercicio
                     if (continuar){
                         choice = true; //Habilita el bucle de opcion de ejercicio
                     }
                     break;
                 case 3:
                     System.out.println("Este ejercicio se queda pa luego");
-                    continuar = desea_continuar(); //Metodo que pregunta si quiere otro ejercicio
+                    continuar = volverMenu(); //Metodo que pregunta si quiere otro ejercicio
                     if (continuar){
                         choice = true; //Habilita el bucle de opcion de ejercicio
                     }
@@ -80,19 +53,57 @@ public class Main
             }
         }
     }
-    //Este metodo deberiamos usar para preguntar constantemente si quiere probar otro ejercicio
-    private static boolean desea_continuar()
-    {
-        System.out.println("\nDesea probar otro ejercicio?\n");
-        System.out.println("Y/N\n");
-        Scanner scanner = new Scanner(System.in);
-        boolean reinicio = true;
-        String a = scanner.nextLine();
-        if (a.equals("N") || a.equals("n"))
-        {
-            reinicio = false;
-        }
-        return reinicio;
+
+    private static void ejercicio2(Scanner scanner, Metodos metodos) {
+        double monto;
+        System.out.println("-----------------------------------");
+        System.out.println("\nEjercicio 2");
+        System.out.println("Ingrese un monto en $$ (double) y se procederá a calcular el IVA del mismo\n");
+        monto = scanner.nextDouble();
+        System.out.println("El monto $"+monto+ " con el IVA aplicado es de $"+ metodos.agregarIVA(monto));
+        System.out.println("-----------------------------------");
     }
 
+    private static void ejercicio1(Scanner scanner, Metodos metodos) {
+        int num1;
+        int num2;
+        System.out.println("-----------------------------------");
+        System.out.println("\nEjercicio 1");
+        System.out.println("\nIngrese 2 numeros. Se procederá a calcular la respectiva Suma, Resta," +
+                " Multiplicacion, Division y Resto de ambos numeros\n");
+        System.out.println("Primer numero: ");
+        num1 = scanner.nextInt();
+        System.out.println("Segundo numero: ");
+        num2 = scanner.nextInt();
+
+        System.out.println("< Resultados >");
+        metodos.operaciones(num1, num2);
+        System.out.println("-----------------------------------");
+    }
+
+    private static boolean menuEjercicio(Scanner scanner, int eleccion){
+        System.out.println("\nQuieres acceder a la consola del ejercicio " + eleccion + "?\n");
+        System.out.println("Y/N\n");
+        String a = scanner.nextLine();
+        a = a.toLowerCase();
+        if (a.equals("y"))
+        {
+            return false; //No reiniciar el bucle
+        }
+        return true;
+    }
+    //Este metodo deberiamos usar para preguntar constantemente si quiere probar otro ejercicio
+    private static boolean volverMenu()
+    {
+        System.out.println("\nDesea probar otro ejercicio ?\n");
+        System.out.println("Y/N\n");
+        Scanner scanner = new Scanner(System.in);
+        String a = scanner.nextLine();
+        a = a.toLowerCase(); // N => n
+        if (a.equals("n"))
+        {
+            return false;
+        }
+        return true;
+    }
 }
